4d6d4e53e740aeafda3c3c20cebdd6310296a7f4
AriAlaniz <86387412+AriAlaniz@users.noreply.github.com>
2021-10-19 19:46:39 -0300

    Update App.js

--- App.js
+++ App.js
@@ -23,3 +23,28 @@ function App() {
 }
 
 export default App;
+
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    const [LastClickDate, setLastClickDate] = useState(new Date());
+  }
+  const onClick = () => {
+    setCount(count + 1);
+    setLastClickDate(new Date());
+  };
+
+  return (
+    <>
+    <h1>Cantidad de productos: {count} </h1>
+    <button onClick={onClick}> + </button>
+    <button onClick={ () => setCount(count - 1)}> - </button>
+    </>
+  )
+
+  export default function App () {
+    return(
+      <>
+      <Counter/>
+      </>
+    );
+  }
